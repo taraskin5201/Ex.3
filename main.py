293b75436bcfe4fbf4d791a1281486ae50a583b9
taraskin5201 <tdumailo@gmail.com>
2023-10-11 21:12:28 +0300

    Середня кількість кроків для чисел від 1 до {N}: {average_steps} added

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import threading
+import queue
 
-# Глобальний лок для доступу до середнього значення кроків
-average_steps_lock = threading.Lock()
-
-# Глобальна змінна для зберігання загальної кількості кроків
-total_steps = 0
+# Глобальний лок для доступу до черги та результатів
+queue_lock = threading.Lock()
+result_queue = queue.Queue()
 
 def collatz_steps(n):
     steps = 0
@@ -16,31 +15,57 @@ def collatz_steps(n):
         steps += 1
     return steps
 
-def calculate_average_steps(start, end):
-    global total_steps
+def calculate_steps(start, end):
+    local_steps = 0
     for num in range(start, end + 1):
         steps = collatz_steps(num)
-        with average_steps_lock:
-            total_steps += steps
+        local_steps += steps
+    with queue_lock:
+        result_queue.put(local_steps)
+
+def worker():
+    while True:
+        start_end = task_queue.get()
+        if start_end is None:
+            break
+        calculate_steps(*start_end)
+        task_queue.task_done()
 
 if __name__ == "__main__":
-    N = 1000  # Замініть це значення на бажану кількість чисел від 1 до N
-    num_threads = 4  # Замініть це значення на бажану кількість потоків
+    N = 1000
+    num_threads = 4
 
-    # Розподіл роботи між потоками
-    thread_list = []
+    # Створення черги завдань
+    task_queue = queue.Queue()
+
+    # Розподіл завдань між потоками
     step = N // num_threads
     for i in range(num_threads):
         start = i * step + 1
         end = (i + 1) * step if i != num_threads - 1 else N
-        thread = threading.Thread(target=calculate_average_steps, args=(start, end))
-        thread_list.append(thread)
-        thread.start()
+        task_queue.put((start, end))
 
-    # Очікування завершення всіх потоків
-    for thread in thread_list:
+    # Створення та запуск потоків
+    threads = []
+    for _ in range(num_threads):
+        thread = threading.Thread(target=worker)
+        thread.start()
+        threads.append(thread)
+
+    # Чекаємо завершення потоків
+    task_queue.join()
+
+    # Завершення роботи потоків
+    for _ in range(num_threads):
+        task_queue.put(None)
+
+    for thread in threads:
         thread.join()
 
-    # Розрахунок середньої кількості кроків
+    # Збираємо результати з черги та розраховуємо середню кількість кроків
+    total_steps = 0
+    while not result_queue.empty():
+        total_steps += result_queue.get()
+
     average_steps = total_steps / N
     print(f"Середня кількість кроків для чисел від 1 до {N}: {average_steps}")
